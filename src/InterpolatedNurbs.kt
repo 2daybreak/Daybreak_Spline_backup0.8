a549bd78e18cba2a589715e09ac4c1c1f5b55d83
2daybreak <41355250+2daybreak@users.noreply.github.com>
2018-08-31 16:13:08 +0900

    Update InterpolatedNurbs.kt

--- src/InterpolatedNurbs.kt
+++ src/InterpolatedNurbs.kt
@@ -11,65 +11,29 @@ class InterpolatedNurbs: Nurbs {
     
     constructor(max: Int, p: MutableList<Vector3>): super(max, p) {
         this.pts = p
+        evalPrm(pts); degree(); order(); evalKnots(); evalCtrlPoints()
+    }
     
     override fun addPts(v: Vector3) {
-        pts.add(v); super.addPts(v); evalCtrlPoints()
+        pts.add(v)
+        evalPrm(pts); degree(); order(); evalKnots(); evalCtrlPoints()
     }
 
     override fun addPts(i: Int, v: Vector3) {
-        pts.add(i, v); super.addPts(i, v); evalCtrlPoints()
+        pts.add(i, v)
+        evalPrm(pts); degree(); order(); evalKnots(); evalCtrlPoints()
     }
 
     override fun removePts(i: Int) {
-        super.removePts(i)
         if(i != -1) pts.removeAt(i);
-        if(!pts.isEmpty()) evalCtrlPoints()
-    }
-
-    override fun evalPrm() {
-        prm.clear()
-        var sum = 0.toDouble()
-        prm.add(sum)
-        //Chord length method
-        for(i in 1 until pts.count())
-        {
-            val del = pts[i] - pts[i - 1]
-            sum += del.length
-        }
-        for(i in 1 until pts.count())
-        {
-            val del = pts[i] - pts[i - 1]
-            prm.add(prm[i - 1] + del.length / sum)
-        }
-    }
-
-    override fun evalKnots() {
-        /*  Evaluate knot vector. For the uniform spacing, e.g.
-        ctrlPts 1 (point)    : n=1,deg=0,order=1,knots={0,1}
-        ctrlPts 2 (linear)   : n=2,deg=1,order=2,knots={0,0,1,1}
-        ctrlPts 3 (quadratic): n=3,deg=2,order=3,knots={0,0,0,1,1,1}
-        ctrlPts 4 (cubic)    : n=4,deg=3,order=4,knots={0,0,0,0,1,1,1,1}
-        ctrlPts 5 (quartic)  : n=5,deg=4,order=5,knots={0,0,0,0,0,1,1,1,1,1}
-        ctrlPts 6 (quintic)  : n=6,deg=5,order=6,knots={0,0,0,0,0,0,1,1,1,1,1,1}
-        ctrlPts 7 (quintic)  : n=7,deg=5,order=6,knots={0,0,0,0,0,0,1/2,1,1,1,1,1,1}
-        ctrlPts 8 (quintic)  : n=8,deg=5,order=6,knots={0,0,0,0,0,0,1/3,2/3,1,1,1,1,1,1}
-        general   (quintic)  : n= ,deg=5,order=6,knots={...,[1/(n-deg),...,(n-order)/(n-deg)],...,} */
-        knots.clear()
-        for(i in 1..order) knots.add(0.toDouble())
-        for(i in 1..order) knots.add(1.toDouble())
-        for(i in 1..pts.size - order) {
-            var interval = 0.0
-            //averaging spacing(reflecting the distribution of prm)
-            for(j in i until i + degree) interval += prm[j]
-            interval /= degree
-            knots.add(degree + i, interval)
-        }
+        if(!pts.isEmpty()) { evalPrm(pts); degree(); order(); evalKnots(); evalCtrlPoints() }
     }
 
     //Algorithm 9.1
     private fun evalCtrlPoints()
     {
         // Evaluate B-spline control points by the given points on a curve
+        ctrlPts.clear()
         val n = pts.size
         val aa  = Array(n) {DoubleArray(n)}
         for (i in pts.indices) {
@@ -85,6 +49,6 @@ class InterpolatedNurbs: Nurbs {
             MatrixSolvLU.ludcmp(n, aa, indx)
             for (j in 0..2) MatrixSolvLU.lubksb(n, aa, indx, bb[j])
         }
-        for (i in pts.indices) ctrlPts[i] = Vector3(bb[0][i], bb[1][i], bb[2][i])
+        for (i in pts.indices) ctrlPts.add(Vector3(bb[0][i], bb[1][i], bb[2][i]))
     }
 }
