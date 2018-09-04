3489b6dca3e121bdbfbd96ccba9417d3dcec8b52
2daybreak <41355250+2daybreak@users.noreply.github.com>
2018-09-04 16:34:09 +0900

    Update InterpolatedBspline.kt

--- InterpolatedBspline.kt
+++ InterpolatedBspline.kt
@@ -57,21 +57,49 @@ class InterpolatedBspline: Bspline {
     //Algorithm 9.1
     private fun generalLU() {
         val n = pts.size
+        if(pts.size <= 2) {
         val aa  = Array(n) {DoubleArray(n)}
-        for (i in pts.indices) {
+        for (i in 0 until n) {
             val span = findIndexSpan(n, prm[i])
             val nn = basisFuncs(span, prm[i])
             for (j in 0..degree) aa[i][span - degree + j] = nn[j]
         }
         val bb = Array(3) {DoubleArray(n)}
-        for (i in pts.indices)
+        for (i in 0 until n)
             for (j in 0..2) bb[j][i] = pts[i][j]
         if (n >= 3) {
             val indx = IntArray(n)
             ludcmp(n, aa, indx)
             for (j in 0..2) lubksb(n, aa, indx, bb[j])
         }
-        for (i in pts.indices) ctrlPts.add(Vector3(bb[0][i], bb[1][i], bb[2][i]))
+        for (i in 0 until n) ctrlPts.add(Vector3(bb[0][i], bb[1][i], bb[2][i]))
+        }
+        else {
+            val np2 = n + 2
+            val a = Array(np2) { DoubleArray(np2) }
+            for (i in 0 until n) {
+                val span = findIndexSpan(np2, prm[i])
+                val nn = basisFuncs(span, prm[i])
+                for (j in 0..degree) aa[i + 1][span - degree + j] = nn[j]
+            }
+            aa[0][0] = 1.0
+            aa[1][0] = -1.0
+            aa[1][1] = 1.0
+            aa[n][n] = -1.0
+            aa[n][n + 1] = 1.0
+            aa[n + 1][n + 1] = 1.0
+            val bb = Array(3) { DoubleArray(np2) }
+            for (j in 0..2) {
+                for(i in 1 until n - 1) bb[j][i + 1] = pts[i][j]
+                bb[j][0] = pts[0][j]
+                bb[j][1] = endDers[0][j] * chord
+                bb[j][n] = endDers[1][j] * chord
+                bb[j][n + 1] = pts[n - 1][j]
+            }
+            val indx = IntArray(np2)
+            ludcmp(np2, aa, indx)
+            for (j in 0..2) lubksb(np2, aa, indx, bb[j])
+            for (i in 0 until np2) ctrlPts.add(Vector3(bb[0][i], bb[1][i], bb[2][i]))
     }
 
     //Algorithm 9.2
